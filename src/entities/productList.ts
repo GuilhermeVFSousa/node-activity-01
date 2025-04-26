ff075936ba7f084a48920464d810048e9d390b92
Gabriel <gabriel.giro.resende@outlook.com>
2025-04-26 11:34:08 -0300

    feat: add output format for ProductList

--- src/entities/productList.ts
+++ src/entities/productList.ts
@@ -7,8 +7,8 @@ export default class ProductList implements Iterable<Product> {
         this.products.push(product);
     }
 
-    async filter(callback: (product: Product) => Promise<boolean>): Promise<Product[]> {
-        const filteredProducts: Product[] = [];
+    async filter(callback: (product: Product) => Promise<boolean>): Promise<ProductList> {
+        const filteredProducts = new ProductList();
         for (const product of this.products) {
             if (await callback(product)) {
                 filteredProducts.push(product);
@@ -16,11 +16,18 @@ export default class ProductList implements Iterable<Product> {
         }
         return filteredProducts;
     }
+
+    toOutputFormat(): Array<Object> {
+        return Array.from(this.products).map((product: Product) => ({
+            id: product.getId(),
+            name: product.getName(),
+          }))
+    }
     
     // Implements the Iterable interface
     *[Symbol.iterator](): Iterator<Product> {
         for (const item of this.products) {
           yield item;
         }
-      }
+    }
 }
